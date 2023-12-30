1e0369f34ae39effacbbcf0e739ed7e30f0784d3
universmc <hubmaster74@gmail.com>
2023-12-30 09:35:06 +0100

    click-update-currentIndex-array-slides

--- assets/script.js
+++ assets/script.js
@@ -15,18 +15,18 @@ const slides = [
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	},
-]
-
+];
+/* ADD eventListener */
 
  let bouton1 = document.getElementById("bouton1");
  bouton1.addEventListener("click", () => {
  	console.log("click OK button slide gauche")
- })
+ });
 
  let bouton2 = document.getElementById("bouton2");
  bouton2.addEventListener("click", () => {
 	console.log("click OK button slide droit")
- })
+ });
 
 
 
@@ -43,3 +43,44 @@ for (let i = 0; i < numberOfSlides; i++) {
 dotsContainer.firstChild.classList.add('dot_selected');
 
 
+/* click arrow */ 
+
+let currentIndex = 0; // Index de l'image actuelle
+
+// Fonction pour mettre à jour le carrousel
+// Fonction pour aller à la slide suivante
+function goToNextSlide() {
+    currentIndex = (currentIndex + 1) % numberOfSlides; // Incrémente currentIndex, en bouclant si nécessaire
+    updateCarousel(); // Met à jour le carrousel
+}
+
+// Fonction pour aller à la slide précédente
+function goToPrevSlide() {
+    currentIndex = (currentIndex - 1 + numberOfSlides) % numberOfSlides; // Décrémente currentIndex, en bouclant si nécessaire
+    updateCarousel(); // Met à jour le carrousel
+}
+
+// Fonction pour mettre à jour le carrousel
+function updateCarousel() {
+    // Mettre à jour l'image et le texte
+    const bannerImg = document.querySelector('.banner-img');
+    bannerImg.src = './assets/images/slideshow/' + slides[currentIndex].image;
+    const taglineDiv = document.querySelector('.arrow-content p');
+    taglineDiv.innerHTML = slides[currentIndex].tagLine;
+
+    // Mettre à jour les bullet points
+	function updateDots() {
+		const dots = document.querySelectorAll('.dot');
+		dots.forEach((dot, index) => {
+			dot.classList.remove('dot_selected'); // Retirer la classe 'dot_selected'
+			if (index === currentIndex) {
+				dot.classList.add('dot_selected'); // Ajouter 'dot_selected' au bullet point actif
+			}
+		});
+	}
+	
+}
+
+// Ajout des event listeners sur les boutons
+bouton1.addEventListener("click", goToPrevSlide); // Écouteur d'événement pour le bouton gauche
+bouton2.addEventListener("click", goToNextSlide); // Écouteur d'événement pour le bouton droit
